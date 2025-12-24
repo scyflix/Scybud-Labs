aac71060f9fe030939ca381075a04c3d6273339b
abdulroqibscybud <abdulroqeeboladipo@gmail.com>
2025-12-24 13:38:39 +0100

    refactor: enhance navigation structure and improve section IDs for better accessibility

--- documentation/js/main.js
+++ documentation/js/main.js
@@ -1,18 +1,27 @@
 import { navLinks } from "./sidebar.js";
-const sections = document.querySelectorAll("article section");
-
+const sections = document.querySelectorAll("section");
 
 navLinks.forEach((link) => {
   link.addEventListener("click", () => {
+
     const targetId = link.getAttribute("href").slice(1);
 
     sections.forEach((section) => {
       section.classList.remove("shown");
     });
 
-    const targetSection = document.getElementById(targetId);
-    if (targetSection) {
-      targetSection.classList.add("shown");
-    } 
-  });
+
+    navLinks.forEach((l) => l.classList.remove("activeLink"))
+link.classList.add("activeLink")
+
+const targetSection = document.getElementById(targetId);
+if (targetSection) {
+  targetSection.classList.add("shown");
+}
+
+
+if(link.hash === window.location.hash) {
+  link.classList.add("activeLink")
+}
+});
 });
